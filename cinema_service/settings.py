92f055183907ca58383883b5952c2bebd33a7a2e
Serhii Budko <sergeybudko.kr@gmail.com>
2025-07-29 21:51:46 +0200

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -66,7 +66,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
@@ -89,14 +90,6 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = 'static/'
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
